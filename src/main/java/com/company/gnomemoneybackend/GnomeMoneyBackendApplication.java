3e7d1bc7acbc24df1be3a8fd0c748daf3d1f760e
Maksolo27 <71383659+Maksolo27@users.noreply.github.com>
2021-09-11 13:17:26 +0300

    Initial commit

---
+++ src/main/java/com/company/gnomemoneybackend/GnomeMoneyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.company.gnomemoneybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnomeMoneyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GnomeMoneyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/gnomemoneybackend/GnomeMoneyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.gnomemoneybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnomeMoneyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
